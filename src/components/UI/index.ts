0fca20e3c66e57adc5f2f1af6c82bcba015e9d59
p10ne <i-smart97@mail.ru>
2021-12-02 19:49:14 +0400

    Input component

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,8 +1,10 @@
 import Icon from './Icon.vue';
 import Tab from './Tab.vue';
 import Checkbox from './Checkbox.vue';
+import Input from './Input.vue';
 export {
   Icon,
   Tab,
-  Checkbox
+  Checkbox,
+  Input
 }
