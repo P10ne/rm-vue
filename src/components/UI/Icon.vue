69b8d4ceaafcd8893bd0d95a74355f71fb847c67
p10ne <i-smart97@mail.ru>
2021-12-02 12:48:07 +0400

    Icon component

---
+++ src/components/UI/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <span :class="{[className]: true}"></span>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    className () {
+      return `icon-${this.name}`;
+    }
+  }
+})
+</script>
