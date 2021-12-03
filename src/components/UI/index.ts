efa19b5effe0df74345336ba1f87d308305e3ddb
p10ne <i-smart97@mail.ru>
2021-12-03 14:51:44 +0400

    Select base

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -2,9 +2,10 @@ import Icon from './Icon.vue';
 import Tab from './Tab.vue';
 import Checkbox from './Checkbox.vue';
 import Input from './Input.vue';
+import Select from './Select/Select.vue';
 export {
   Icon,
   Tab,
   Checkbox,
-  Input
+  Select,
 }
