aaec619871ba9cf3845ae80b9f70b1080a2c8ecd
p10ne <i-smart97@mail.ru>
2021-12-06 19:32:09 +0400

    Button/IconButton component

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,14 +1,17 @@
-import Icon from './Icon.vue';
-import Tab from './Tab.vue';
-import Checkbox from './Checkbox.vue';
-import Input from './Input.vue';
-import Select from './Select/Select.vue';
-import SelectOption from './Select/SelectOption.vue';
+import Icon from './Icon.vue'
+import Tab from './Tab.vue'
+import Checkbox from './Checkbox.vue'
+import Select from './Select/Select.vue'
+import SelectOption from './Select/SelectOption.vue'
+import Button from './Button/Button.vue'
+import IconButton from './Button/IconButton.vue'
 
 export {
   Icon,
   Tab,
   Checkbox,
   Select,
-  SelectOption
+  SelectOption,
+  Button,
+  IconButton
 }
